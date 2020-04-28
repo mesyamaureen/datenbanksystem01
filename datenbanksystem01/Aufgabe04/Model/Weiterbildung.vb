fbdae8859843b8ba496c631eb7104436baa3e65a
Hochtritt <hochtritt@berlin.strato.de>
2020-04-28 11:34:47 +0200

    Fachkonzeptschicht

---
+++ datenbanksystem01/Aufgabe04/Model/Weiterbildung.vb
@@ -0,0 +1,19 @@
+﻿Public Class Weiterbildung
+
+    'Properties
+    Public Property intID As Integer
+    Public Property strBezeichnung As String
+    Public Property strThema As String
+    Public Property strCurriculum As String
+
+
+
+
+
+
+
+
+
+
+
+End Class
---
+++ datenbanksystem01/Aufgabe04/Model/fachkonzept.vb
@@ -0,0 +1,12 @@
+﻿Public Class fachkonzept
+
+    'Properties
+
+
+
+
+    'bekommt übergeben die parameter des Kurses
+    'alle Use Case funktionen
+
+
+End Class
