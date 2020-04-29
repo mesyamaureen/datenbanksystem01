3a32da9001385e3edf3fe9309507e6d6631e45f7
Mesya Maureen <mesyamaureen@gmail.com>
2020-04-29 14:53:26 +0200

    Eriegnisprozedur frmStart erstellt

--- datenbanksystem01/Aufgabe04/Form/frmStart.designer.vb
+++ datenbanksystem01/Aufgabe04/Form/frmStart.designer.vb
@@ -23,19 +23,9 @@ Partial Class frmStart
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.btnBeenden = New System.Windows.Forms.Button()
         Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.SuspendLayout()
-        '
-        'MenuStrip1
-        '
-        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1258, 24)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
         '
         'btnBeenden
         '
@@ -62,17 +52,12 @@ Partial Class frmStart
         Me.CancelButton = Me.btnBeenden
         Me.ClientSize = New System.Drawing.Size(1258, 968)
         Me.Controls.Add(Me.btnBeenden)
-        Me.Controls.Add(Me.MenuStrip1)
-        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "frmStart"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Weiterbildung time2-Akademie"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents btnBeenden As Button
     Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
 End Class
--- datenbanksystem01/Aufgabe04/Form/frmStart.vb
+++ datenbanksystem01/Aufgabe04/Form/frmStart.vb
@@ -1,13 +1,46 @@
 ﻿Public Class frmStart
-    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs)
+    ''' <summary>
+    ''' Beim Anzeigen des Startfensters wird die Anmeldung durchgeführt.
+    ''' Nach der Anmeldung wird das Fenster geschlossen und in die weiteren Hauptfenster verzweigt.
+    ''' Wird die Anmeldung abgebrochen, endet die Anwendung.
+    ''' </summary>
+
+    Private Sub frmStart_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        'Deklaration
+        Dim strBenutzername As String 'im Anmeldedialog übergegeben werden
+        Dim strPasswort As String 'im Anmeldedialog übergegeben werden
+
+        'Initialisierung
+        Aufgabe04.AktuellAngemeldeterBenutzer = Nothing 'Aktuell gibt es keinen angemeldeten Benutzer
+
+        'Aufrufen anderer Prozedur
+
+
+        'Nachprüfende Schleife 
+        Do dlgAnmeldung.ShowDialog() 'Anmeldedialog anzeigen
+
+            'Prüfen, ob der Dialog mit OK geschlossen wurde
+            If Not dlgAnmeldung.DialogResult = Windows.Forms.DialogResult.OK Then
+                'wenn nicht, dann Fenster schliessen und damit endet die Anwendung
+                Me.Close()
+                Exit Sub 'Prozedur verlassen, damit die Anwendung hier endet
+            End If
+
+            'Benutzername und Passwort von dem Dialogfenster ermitteln
+            strBenutzername = dlgAnmeldung.txtBenutzername.Text
+            strPasswort = dlgAnmeldung.txtBenutzername.Text
+
+            'Benutzer mit der eingegebenen Kombination aus Benutzername und Passwort finden
+            For Each
+
+
+
+
 
     End Sub
-
-    Private Sub AKtivToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
     Private Sub btnBeenden_Click(sender As Object, e As EventArgs) Handles btnBeenden.Click
         Me.Close()
     End Sub
+
+    P
 End Class
