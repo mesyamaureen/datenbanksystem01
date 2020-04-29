52deadf1f23e2a93db65149939ce4560190f8a7b
Hochtritt <hochtritt@berlin.strato.de>
2020-04-29 23:42:51 +0200

    details database

--- datenbanksystem01/Aufgabe04/Form/testLogic.vb
+++ datenbanksystem01/Aufgabe04/Form/testLogic.vb
@@ -1,15 +1,30 @@
 ﻿Public Class testLogic
     Private Sub testLogic_Click(sender As Object, e As EventArgs) Handles MyBase.Click, btnLogIn.Click
-        Dim strDBenutzername As String = "jeynie"
-        Dim strDPasswort As String = "pw"
+        'Dim strDBenutzername As String = "jeynie"
+        'Dim strDPasswort As String = "pw"
 
-        initialise()
-        Dim versuch As Dictionary(Of String, String) = logIn(strDBenutzername, strDPasswort)
+        '   initialise()
+        'Dim versuch As Dictionary(Of String, String) = logIn(strDBenutzername, strDPasswort)
 
-        lblattempt.Text = versuch("attempt")
-        lblrole.Text = versuch("role")
+        '   lblattempt.Text = versuch("attempt")
+        '  lblrole.Text = versuch("role")
 
-        ' Console.WriteLine("blabliblub")
+
+        Dim appPath As String = Application.StartupPath()
+        Dim connect As New SQLite.SQLiteConnection
+        connect.ConnectionString = "Data Source=" + appPath + "\user.s3db;"
+        connect.Open()
+        'Command = connect.CreateCommand
+        'Command.commandText
+        connect.Close()
+
+
+        Console.WriteLine(appPath)
+
 
     End Sub
+
+
+
+
 End Class
