daf9b7f5aca11357352a23308a1e63826935213c
Mesya Maureen <mesyamaureen@gmail.com>
2020-04-30 01:04:19 +0200

    Form Gruss und GrussMitarbeiter bearbeitet

--- datenbanksystem01/Aufgabe04/Form/frmGruss.vb
+++ datenbanksystem01/Aufgabe04/Form/frmGruss.vb
@@ -2,21 +2,19 @@
     Private Sub frmGruss_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         'Deklaration
         Dim strBegruessungKunde As String
-        Dim strBegruessungMitarbeiter
         Dim strVorname As String
         Dim strNachname As String
 
         'Initialisierung
-        'strVorname = Logic.AktuellAngemeldeterBenutzer.Vorname 'TODO: Uncomment
-        'strNachname = Logic.AktuellAngemeldeterBenutzer.Nachname 'TODO: Uncomment
+        strVorname = Logic.AktuellAngemeldeterBenutzer.strVorname
+        strNachname = Logic.AktuellAngemeldeterBenutzer.strName
+
         'zwei verschiedene Begrüßungen werden aus Vorname und Nachname zusammengebaut
         strBegruessungKunde = "Willkommen " & strVorname & strNachname & "! " & vbCrLf &
                               "Sie sind als Kunde angemeldet."
 
-        strBegruessungMitarbeiter = "Wilkommen " & strVorname & strNachname & "! " & vbCrLf &
-                                    "Sie sind als Mitarbeiter angemeldet."
-
         'Ausgabe im Meldungsfenster 'TODO: Connection to the result of Login
+        MsgBox(strBegruessungKunde, MsgBoxStyle.OkOnly, "Wilkommen")
 
 
 
--- datenbanksystem01/Aufgabe04/Form/frmGrussMitarbeiter.vb
+++ datenbanksystem01/Aufgabe04/Form/frmGrussMitarbeiter.vb
@@ -1,3 +1,20 @@
 ﻿Public Class frmGrussMitarbeiter
+    Private Sub frmGrussMitarbeiter_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        'Deklaration
+        Dim strBegruessungMitarbeiter As String
+        Dim strVorname As String
+        Dim strNachname As String
 
+        'Initialisierung
+        strVorname = Logic.AktuellAngemeldeterBenutzer.strVorname
+        strNachname = Logic.AktuellAngemeldeterBenutzer.strName
+
+        'eine Begrüßung wird aus Vorname und Nachname zusammengebaut
+        strBegruessungMitarbeiter = "Willkommen " & strVorname & strNachname & "! " & vbCrLf &
+                                    "Sie sind als Mitarbeiter angemeldet."
+
+        'Anzeige der Begrüßung in einem Meldungsfenster
+        MsgBox(strBegruessungMitarbeiter, MsgBoxStyle.OkOnly, "Willkommen")
+
+    End Sub
 End Class
